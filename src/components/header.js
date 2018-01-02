be835bd1ef91cea35f402d1acb4a5dcb47ecfe96
Ed Lewis <ed.lewis@gmail.com>
2018-01-02 11:30:40 -0500

    better image

--- src/components/header.js
+++ src/components/header.js
@@ -3,11 +3,10 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <header class="header">
-        <div class='header__text-box'>
-          <h1 class='heading-primary'>
-            <span class='heading-primary-main'>ed lewis</span>
-            <span class='heading-primary-sub'>making things</span>
+      <header className="header">
+        <div className='header__text-box'>
+          <h1 className='heading-primary'>
+            <span className='heading-primary-main'>ed lewis</span>
           </h1>
         </div>
       </header>
